a19acc93d1505d08c46fbadd99e112d4d0df0908
Anthony D. Blaom <anthony.blaom@gmail.com>
2021-01-22 11:10:23 +1300

    fix invalid tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -45,34 +45,34 @@ end
 
 @testset "docstring" begin
     @test docstring(Foo(1,'x')) == "Foo"
-    @test docstring(Fruit.RedApple()) == "Main.Fruit.RedApple"
+    @test docstring(Fruit.RedApple(4)) == "Main.Fruit.RedApple{Int64}"
 end
 
 @testset "hyperparameter_ranges" begin
     @test hyperparameter_ranges(Foo(1, 'x')) == (nothing, nothing)
-    @test hyperparameter_ranges(Fruit.RedApple()) == ()
+    @test hyperparameter_ranges(Fruit.RedApple(4)) == (nothing, )
 end
 
 @testset "name" begin
     @test name(Float64) == "Float64"
     @test name(Fruit.RedApple) == "RedApple"
-    @test name(Fruit.RedApple()) == "RedApple"
+    @test name(Fruit.RedApple(4)) == "RedApple"
 end
 
 @testset "human_name" begin
     @test human_name(AbstractString) == "abstract string"
     @test human_name(Fruit.RedApple) == "red apple"
-    @test human_name(Fruit.RedApple()) == "red apple"
+    @test human_name(Fruit.RedApple(4)) == "red apple"
 end
 
 @testset "hyperparameters" begin
     @test hyperparameters(Foo(1, 'x')) == (:x, :y)
-    @test hyperparameters(Fruit.RedApple()) == ()
+    @test hyperparameters(Fruit.RedApple(4)) == (:x, )
 end
 
 @testset "hyperparameter_types" begin
     @test hyperparameter_types(Foo(1, 'x')) == ("Int64", "Char")
-    @test hyperparameter_types(Fruit.RedApple()) == ()
+    @test hyperparameter_types(Fruit.RedApple(4)) == ("Int64", )
 end
 
 
@@ -88,7 +88,7 @@ const NONCONSTANT = [:docstring,
 @testset "traits with constant fall-back" begin
     for trait in setdiff(StatisticalTraits.TRAITS, NONCONSTANT)
         ex = quote
-            a = $trait(Fruit.RedApple())
+            a = $trait(Fruit.RedApple(4))
             b = $trait(Foo(1, 'x'))
             if ismissing(a)
                 @test ismissing(b)
